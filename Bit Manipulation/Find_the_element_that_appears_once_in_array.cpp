765da4aeeea75150f91206f3f99683845c948a58
Dhara Solanki <83348790+Dhara3078@users.noreply.github.com>
2021-06-07 20:31:35 +0530

    Update Find_the_element_that_appears_once_in_array.cpp

--- Bit Manipulation/Find_the_element_that_appears_once_in_array.cpp
+++ Bit Manipulation/Find_the_element_that_appears_once_in_array.cpp
@@ -1,1 +1,13 @@
+// link for question :
+// https://practice.geeksforgeeks.org/problems/element-appearing-once2552/1
 
+
+int search(int A[], int N){
+	    //code
+	    int res=0;
+	    for(int i=0;i<N;i++)
+	    {
+	        res^=A[i]; // using Xor operator to remove repeating element // Xor properties : 0^N=N , N^N=0
+	    }
+	    return res;
+	}

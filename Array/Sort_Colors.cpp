6cce5156664b5875cee6904767a080779be97679
Dhara Solanki <83348790+Dhara3078@users.noreply.github.com>
2021-06-09 22:43:41 +0530

    Create Sort_Colors.cpp

---
+++ Array/Sort_Colors.cpp
@@ -0,0 +1,19 @@
+// link for question⬇⬇
+//https://leetcode.com/problems/sort-colors/
+
+// Used Dutch National Flag Algorithm
+void sortColors(vector<int>& nums) {
+        int low=0,high=nums.size()-1,mid=0;
+        
+        while(mid<=high){
+           if(nums[mid]==0){
+               swap(nums[mid++],nums[low++]);
+             }  
+           else if(nums[mid]==1){
+               mid++;
+           }
+           else {
+               swap(nums[mid],nums[high--]);
+           }
+        }
+    }

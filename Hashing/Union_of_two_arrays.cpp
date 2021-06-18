846e3f84eefd1533c410fa32ea44d09ed82625b2
Dhara Solanki <83348790+Dhara3078@users.noreply.github.com>
2021-06-18 17:56:15 +0530

    Update Union_of_two_arrays.cpp

--- Hashing/Union_of_two_arrays.cpp
+++ Hashing/Union_of_two_arrays.cpp
@@ -1,4 +1,7 @@
+//Question link⬇⬇
+// https://practice.geeksforgeeks.org/problems/union-of-two-arrays3538/1#
 //Function to return the count of number of elements in union of two arrays.
+
     int doUnion(int a[], int n, int b[], int m)  {
         //hashing method
         unordered_set<int> S; 

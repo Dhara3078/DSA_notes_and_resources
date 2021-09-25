c88934434955fef8b54610836793954ed92c1073
Dhara Solanki <83348790+Dhara3078@users.noreply.github.com>
2021-09-25 15:37:49 +0530

    Day 24

--- sorting/sorting algorithms/Insertion_Sort.cpp
+++ sorting/sorting algorithms/Insertion_Sort.cpp
@@ -9,7 +9,7 @@
 //Used for smaller values of N and works good when array is partially sorted
 
 
-void InsertionSort(int arr[] , int n){
+void InsertionSort(int arr[], int n){
   for(int i=0;i<n-1;i++){
     for(int j=i+1;j>0;j--){
       if(arr[j]<arr[j-1])

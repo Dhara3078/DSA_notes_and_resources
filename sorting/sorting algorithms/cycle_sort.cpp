3c3f227107acced56fa56c9480d3888b89f92981
Dhara Solanki <83348790+Dhara3078@users.noreply.github.com>
2021-09-25 11:17:47 +0530

    Create cycle_sort.cpp

---
+++ sorting/sorting algorithms/cycle_sort.cpp
@@ -0,0 +1,18 @@
+//This sorting algorithm is used when given numbers from range 1 to N (continues numbers)
+//Idea : We are running while loop till n-1 and we check if index i is not equal to arr[i]-1 then swap arr[i] and arr[arr[i]-1]
+//       else do i++
+//       we are running loop till n-1 because if 1 to n-1 elements are in sorted order then last last element will alse be at sorted order
+//Time complexity : Wrost case = O(N)
+//                  Average case = O(N)
+//                  Best case = O(N)
+
+
+void cycleSort (int arr[],int n){
+  int i=0;
+  while(i<n-1){
+    if(i!=arr[i]-1)
+      swap(arr[i],arr[arr[i]-1]);
+    else
+      i++;
+  }
+}

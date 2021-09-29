57d2be29d7f2899ce8dacd9a6c3fb6e1de302737
Dhara Solanki <83348790+Dhara3078@users.noreply.github.com>
2021-09-29 14:54:10 +0530

    Day 38

---
+++ Matrix/Search a 2D Matrix.cpp
@@ -0,0 +1,18 @@
+//Link for the question⬇⬇
+//https://leetcode.com/problems/search-a-2d-matrix/
+
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int i=0,j=matrix[0].size()-1;
+        while(i<matrix.size() && j>=0){
+            if(matrix[i][j]==target){
+                return true;
+            }
+            else if (matrix[i][j]>target)
+                j--;
+            else
+                i++;
+        }
+        
+        return false;
+    }

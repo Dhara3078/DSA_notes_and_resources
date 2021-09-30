79b05c540c7d4c48d840b826933552fb507846c1
Dhara Solanki <83348790+Dhara3078@users.noreply.github.com>
2021-09-30 18:24:57 +0530

    Day 41

--- Stack/Implement two stacks in an array.cpp
+++ Stack/Implement two stacks in an array.cpp
@@ -1,25 +1,46 @@
 //Link for the question⬇⬇
-//https://practice.geeksforgeeks.org/problems/delete-middle-element-of-a-stack/1#
+//https://practice.geeksforgeeks.org/problems/implement-two-stacks-in-an-array/1
 
 
-//Function to delete middle element of a stack.
-    //idea is to use another auxiliary stack and push all element till middle element of main stack
-    //after that pop middle element from top of auxiliary stack
-    //now push all element of auxiliary stack element to main stack
-    void deleteMid(stack<int>&s, int sizeOfStack)
-    {
-       stack<int> aux;
-        int mid = sizeOfStack>>1;
-        for(int i=0; i<mid; i++)
-        {
-            aux.push(s.top());
-            s.pop();
-        }
-        s.pop();
-        for(int i=0; i<mid; i++)
-        {
-            s.push(aux.top());
-            aux.pop();
-        }
-            
-    }
+//Function to push an integer into the stack1.
+void twoStacks :: push1(int x)
+{
+     if(top1<top2-1){
+         top1++;
+         arr[top1]=x;
+     }
+}
+   
+//Function to push an integer into the stack2.
+void twoStacks ::push2(int x)
+{
+     if(top1<top2-1){
+         top2--;
+         arr[top2]=x;
+     }
+}
+   
+//Function to remove an element from top of the stack1.
+int twoStacks ::pop1()
+{
+      if(top1>=0){
+          int x=arr[top1];
+          top1--;
+          return x;
+      }else{
+          return -1;
+      }
+}
+
+//Function to remove an element from top of the stack2.
+int twoStacks :: pop2()
+{
+      if(top2<size){
+          int x=arr[top2];
+          top2++;
+          return x;
+      }else{
+          return -1;
+      }
+}
+

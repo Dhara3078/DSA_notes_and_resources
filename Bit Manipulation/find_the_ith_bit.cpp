df617f04fd1fad24604ae65b176156545ea3a036
Dhara Solanki <83348790+Dhara3078@users.noreply.github.com>
2021-10-04 12:17:17 +0530

    Day 47

--- Bit Manipulation/find_the_ith_bit.cpp
+++ Bit Manipulation/find_the_ith_bit.cpp
@@ -10,3 +10,19 @@ int find_ithBit(int n, int i){
   else
     return 0;
 }
+
+
+//Same question but because in tquestion it mention that from left side index start with 0, we need to change code little 
+//Link for the question⬇⬇
+//https://practice.geeksforgeeks.org/problems/check-whether-k-th-bit-is-set-or-not-1587115620/1
+
+ // Function to check if Kth bit is set or not.
+    bool checkKthBit(int n, int k)
+    {
+        // Your code here
+        // It can be a one liner logic!! Think of it!!
+        if(n&(1<<k))
+            return true;
+        else
+            return false;
+    }

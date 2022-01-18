17d8e2f920308d349ef5774565f621d9be6b4d94
Dhara Solanki <83348790+Dhara3078@users.noreply.github.com>
2022-01-18 18:30:30 +0530

    Update Find the Duplicate Number.cpp

--- Array/Find the Duplicate Number.cpp
+++ Array/Find the Duplicate Number.cpp
@@ -1,6 +1,10 @@
 //link for question⬇⬇
 //https://leetcode.com/problems/find-the-duplicate-number/
 
+
+//method 1: using extra space (vector) 
+//Time complexity : o(n)
+//space complexity : o(n)
 int findDuplicate(vector<int>& nums) {
         //idea is to make temp vector and add nums vector's element in its index and if we find that temp element 
         //already have some value we return that value 
@@ -14,5 +18,26 @@ int findDuplicate(vector<int>& nums) {
             else                    // if temp vector's element is not zero that means it's a repeating number
                 return nums[i-1];
         }
+        return -1;
+    }
+
+//method 2: using cycle sort
+//idea is to use cyclic sort 
+//just do one modification in cyclic sort
+//add extra candition for duplicates
+//Time complexity : o(n)
+//space complexity : o(1)
+    int findDuplicate(vector<int>& nums) {
+        int i=0;
+        while(i<nums.size()){
+           if(i!=nums[i]-1 ){
+             if(nums[i]!=nums[nums[i]-1]) //this condition is for duplicates 
+                            swap(nums[i],nums[nums[i]-1]);
+                        else
+                            return nums[i];
+                    }
+                    else
+                      i++;
+                  }
         return -1;
     }

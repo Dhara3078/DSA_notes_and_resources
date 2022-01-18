3fb91bdf7677b518a1c76debe0ad11198ef63122
Dhara Solanki <83348790+Dhara3078@users.noreply.github.com>
2022-01-18 18:17:08 +0530

    Update Cyclically_rotate_an_array_by_one.cpp

--- Array/Cyclically_rotate_an_array_by_one.cpp
+++ Array/Cyclically_rotate_an_array_by_one.cpp
@@ -1,6 +1,10 @@
 //link for question⬇
 //https://practice.geeksforgeeks.org/problems/cyclically-rotate-an-array-by-one2614/1
+//Time complexity : 0(n) where n = number of array element
 
+//Step 1: store last element of array
+//Step 2: run loop from right to left expect first element and assign value of i-1 element's to i index element
+//step 3: now assign stored last element of array to first element of array
 void rotate(int arr[], int n)
 {
     int temp=arr[n-1];

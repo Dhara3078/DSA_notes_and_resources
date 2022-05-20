138dbbc1b35928b8ab26636b6eac8f423d4dd5d2
Dhara Solanki <83348790+Dhara3078@users.noreply.github.com>
2022-05-20 15:15:06 +0530

    Create Sort Colors.cpp

---
+++ DSA Sheet by Arsh (Solved que by me)/Array/Sort Colors.cpp
@@ -0,0 +1,25 @@
+//Link for the question
+//https://leetcode.com/problems/sort-colors/
+
+//Time Complexity : O(n)
+//Space Complexity : O(1)
+
+class Solution {
+public:
+    //Dutch National Flag ALgo
+    void sortColors(vector<int>& nums) {
+        int low=0,high=nums.size()-1,mid=0;
+        
+        while(mid<=high){
+           if(nums[mid]==0){
+               swap(nums[mid++],nums[low++]);
+             }  
+           else if(nums[mid]==1){
+               mid++;
+           }
+           else {
+               swap(nums[mid],nums[high--]);
+           }
+        }
+    }
+};

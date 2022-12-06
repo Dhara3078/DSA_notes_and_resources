34f9ca0ed337756f5fb42853d849b7439a5f51e9
Dhara Solanki <83348790+Dhara3078@users.noreply.github.com>
2022-12-06 11:11:50 +0530

    Day 6(medium)

---
+++ LeetCode Daily Challenge/Dec22/Day 6(Odd Even Linked List).cpp
@@ -0,0 +1,25 @@
+//Link for the question⬇⬇
+//https://leetcode.com/problems/odd-even-linked-list/description/
+
+//Medium level question 
+
+//Code:
+
+ListNode* oddEvenList(ListNode* head) {
+        ListNode *odd = NULL, *headodd = NULL, *even = NULL, *headeven = NULL;
+        
+        if(!head || !head->next || !head->next->next) return head;
+         
+        odd = head; headodd = odd;
+        even = head->next; headeven = even;
+        
+        while(even && even->next){
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
+        }
+        
+        odd->next = headeven;
+        return headodd;
+    }  
